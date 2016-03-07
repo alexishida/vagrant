7249b961fbb68b7424718b3a5b14cffff36dfc2f
Alex Ishida <alexishida@gmail.com>
2016-03-07 15:41:35 -0400

    update nvm and node version

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -2,12 +2,12 @@
 
 sudo apt-get install -y build-essential libssl-dev
 
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 
 
-sudo su vagrant -c '. ~vagrant/.nvm/nvm.sh;nvm install v5.1.0'
-sudo su vagrant -c '. ~vagrant/.nvm/nvm.sh;nvm use v5.1.0'
-sudo su vagrant -c '. ~vagrant/.nvm/nvm.sh;nvm alias default v5.1.0'
+sudo su vagrant -c '. ~vagrant/.nvm/nvm.sh;nvm install v5.7.1'
+sudo su vagrant -c '. ~vagrant/.nvm/nvm.sh;nvm use v5.7.1'
+sudo su vagrant -c '. ~vagrant/.nvm/nvm.sh;nvm alias default v5.7.1'
 sudo su vagrant -c '. ~vagrant/.nvm/nvm.sh;nvm use default'
 
 echo 'export NVM_DIR="/home/vagrant/.nvm"' >> /home/vagrant/.bash_profile
